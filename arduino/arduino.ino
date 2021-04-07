9e153c7400417bd4dd0a5d0df6cd5ec74d5f3df9
Bruno Orlandin <brunoorlandin35@gmail.com>
2021-04-07 20:54:17 -0300

    dev arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
 
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+  short int httpResponseCode;
+  const char* ssid = "";
+  const char* password =  "";
+  String url;
+  String httpRequestData,payload;
+  bool button=false,aux=false;
+  
+void Conectar(){
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("IP: " + (String)WiFi.localIP());
+};
+
+void Mudar() {
+  button = !button;
+  if (button) aux = !aux;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(pinButton), Mudar, CHANGE);
+  
 }
 
 void loop() {
